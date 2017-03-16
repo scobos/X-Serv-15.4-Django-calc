2b90da814f8727ff982aded188932475b52dc789
Sergio Cobos <SergioCobos@MacBook-Pro-de-Cobos.local>
2017-03-16 18:46:27 +0100

    Ejercicio 15.4

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+import calc.views
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    
+    url(r'(\d+)\+(\d+)', calc.views.suma, name="sumar"),
+    url(r'(\d+)\-(\d+)', calc.views.resta, name="restar"),
+    url(r'(\d+)\*(\d+)', calc.views.multiplica, name="multiplicar"),
+    url(r'(\d+)/(\d+)', calc.views.divide, name="dividir"),
     url(r'^admin/', include(admin.site.urls)),
 )
