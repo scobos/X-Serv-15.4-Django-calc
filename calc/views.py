f82c0929c7dc10c434c0b158fcc80f9f88c01006
Sergio Cobos <SergioCobos@MacBook-Pro-de-Cobos.local>
2017-03-16 18:46:13 +0100

    Ejercicio 15.4

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def suma(request, op1, op2):
+    return HttpResponse('<h1>Sumar:</h1>' + op1 + '+'+ op2 + '=' + str(int(op1) + int(op2)))
+
+def resta(request, op1, op2):
+    return HttpResponse('<h1>Restar:</h1>' + op1 + '-'+ op2 + '=' + str(int(op1) - int(op2)))
+
+def multiplica(request, op1, op2):
+    return HttpResponse('<h1>Multiplicar:</h1>' + op1 + '*'+ op2 + '=' + str(int(op1) * int(op2)))
+
+def divide(request, op1, op2):
+    return HttpResponse('<h1>Dividir:</h1>' + op1 + '/'+ op2 + '=' + str(int(op1) / int(op2)))
